208df03842aec11939c5e0a4091fafc57599102a
Nurlyssultan <nurlyssultan.sabitov@gmail.com>
2019-06-11 00:55:14 +0600

    Added ListView,DetailView for project to fetch

--- QuotesApp/basic_app/urls.py
+++ QuotesApp/basic_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from basic_app import views
 urlpatterns = [
     path('quotes/', views.QuoteListView.as_view(), name='quotes'),
+    path('quotes/<int:pk>/', views.QuoteDetailView.as_view(), name='detail'),
 ]
--- QuotesApp/basic_app/views.py
+++ QuotesApp/basic_app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
-from basic_app.models import QuotePost
+from django.views.generic import View,TemplateView,ListView,DetailView
+from basic_app.models import QuotePost,QuoteAuthor
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'basic_app/index.html'
-class QuoteListView(TemplateView):
+class QuoteListView(ListView):
+    context_object_name = 'quotes'
+    model = QuotePost
     template_name = 'basic_app/quote_list.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['quotes'] = QuotePost.objects.order_by('dateCreation')
-        return context
+class QuoteDetailView(DetailView):
+    context_object_name = 'quote_detail'
+    model = QuotePost
+    template_name = 'basic_app/quote_detail.html'
+class AuthorListView(ListView):
+    context_object_name = 'authors'
+    model = QuoteAuthor
+    template_name = 'basic_app/author_list.html'
+class AuthorDetailView(DetailView):
+    context_object_name = 'author_detail'
+    model = QuoteAuthor
+    template_name = 'basic_app/author_quotes.html'
